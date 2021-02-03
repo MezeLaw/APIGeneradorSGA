4f9e0efe3fbc02e725c2ac26113f04fbc2903bd5
unknown <mezequielabogado@gmail.com>
2021-02-03 00:52:51 -0300

    Se elimina HomeController que no era utilizado y se reconfigura corectamente el RotulosController como /api/xxx. De todos modos entiendo que un nuevo controller deberia no tener problemas al mapearse tambien como /api/xxx.

--- src/main/java/com/meze/APIGeneradorSGA/controller/RotulosController.java
+++ src/main/java/com/meze/APIGeneradorSGA/controller/RotulosController.java
@@ -16,12 +16,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.meze.APIGeneradorSGA.dto.Elemento;
 import com.meze.APIGeneradorSGA.enums.CodigosPDF;
 
 @CrossOrigin
+@RequestMapping("/api")
 @RestController
 public class RotulosController {
 
